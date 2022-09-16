305692f8f05f455d9a43020fd3b5d7906a31be15
Hene Chen <henecastro@gmail.com>
2022-09-16 04:19:30 -0300

    Site desenvolvido juntamente com a semana front-end da DIO

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyDYBg4RoQaLCfc8AHGbOt9O_HOflAZdfjs",
+authDomain: "projeto-teste-f36d2.firebaseapp.com",
+projectId: "projeto-teste-f36d2",
+storageBucket: "projeto-teste-f36d2.appspot.com",
+messagingSenderId: "124887601289",
+appId: "1:124887601289:web:931477c195934a4472f658"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
